38e5a9b4120f0ef0d387f17c785697fa85d43023
Parth Mangukiya <mparth141@gmail.com>
2024-07-12 13:27:09 +0530

    manage download cnts

--- api_services/cron/index.php
+++ api_services/cron/index.php
@@ -96,7 +96,6 @@ if ($live_apps != null && is_array($live_apps) && count($live_apps) > 0) {
         // $html = file_get_contents('https://play.google.com/store/apps/details?id=com.allsocialvideos.multimedia.videodlpro');
         $http_response_code = $http_response_header[0];
         if (strpos($http_response_code, '200') !== false) {
-            
 
         } else {
             $data = array(
@@ -116,6 +115,35 @@ if ($live_apps != null && is_array($live_apps) && count($live_apps) > 0) {
             );
             $db->insert('tbl_notification', $noti_data);
         }
+    }
+}
+
+$qry_live_apps = "SELECT id, app_name, package_name, file FROM tbl_apps WHERE is_deleted = 0";
+$live_apps = $db->execute($qry_live_apps);
+if ($live_apps != null && is_array($live_apps) && count($live_apps) > 0) {
+    foreach($live_apps as $app){
+
+        // Update Download Count
+        $qry_cnt = "select IFNULL((SELECT count(DISTINCT u.id) FROM tbl_app_users as u WHERE u.app_id = '" . $app['id'] . "'),0) AS total_cnt, 
+        IFNULL((SELECT count(DISTINCT u.id) FROM tbl_app_users as u WHERE u.app_id = '" . $app['id'] . "' AND DATE_FORMAT(u.entry_date, '%Y-%m-%d') = '".date('Y-m-d')."' ),0) AS today_cnt, 
+        IFNULL((SELECT count(DISTINCT u.id) FROM tbl_app_users as u WHERE u.app_id = '" . $app['id'] . "' AND DATE_FORMAT(u.entry_date, '%Y-%m-%d') = '".date("Y-m-d", strtotime("-1 day"))."' ),0) AS yestarday_cnt";
+        $res_cnt = $db->execute($qry_cnt);
+        $total_cnt = 0;
+        $today_cnt = 0;
+        $yestarday_cnt = 0;
+        if ($res_cnt != null && is_array($res_cnt) && count($res_cnt) > 0) {
+            $cnts = $res_cnt[0];
+            $total_cnt = $cnts['total_cnt'];
+            $today_cnt = $cnts['today_cnt'];
+            $yestarday_cnt = $cnts['yestarday_cnt'];
+        }
+        $data = array(
+            "total_cnt" => $total_cnt,
+            "today_cnt" => $today_cnt,
+            "yestarday_cnt" => $yestarday_cnt,
+        );
+        $db->update('tbl_apps', $data, array("id" => $app['id']));
+
     }
 }
 ?>
